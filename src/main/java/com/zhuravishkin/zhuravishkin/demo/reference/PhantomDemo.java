18c3b7c0de6069af47a7b033602644c66c97d6f2
alexey.zhuravishkin <zhuravishkin@gmail.com>
2022-09-15 02:20:05 +0300

    JavaRush redo - collections - reference

---
+++ src/main/java/com/zhuravishkin/zhuravishkin/demo/reference/PhantomDemo.java
@@ -0,0 +1,27 @@
+package com.zhuravishkin.zhuravishkin.demo.reference;
+
+import java.lang.ref.PhantomReference;
+import java.lang.ref.Reference;
+import java.lang.ref.ReferenceQueue;
+import java.util.ArrayList;
+
+public class PhantomDemo {
+    public static void main(String[] args) {
+        ReferenceQueue<Integer> queue = new ReferenceQueue<Integer>();
+
+        ArrayList<PhantomReference<Integer>> list = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            Integer x = Integer.valueOf(i);
+            list.add(new PhantomReference<>(x, queue));
+        }
+
+        System.gc();
+
+        Reference<? extends Integer> referenceFromQueue;
+        while ((referenceFromQueue = queue.poll()) != null) {
+            System.out.println(referenceFromQueue.get());
+            referenceFromQueue.clear();
+        }
+    }
+}
