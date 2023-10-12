6a0e3b4bfe75b969f6eccf63b1a681870a111375
Aleksey Zhuravishkin <zhuravishkin@gmail.com>
2023-10-12 15:46:08 +0300

    added multithreading

---
+++ src/main/java/com/zhuravishkin/zhuravishkin/stepik/multithreading/T3Point.java
@@ -0,0 +1,27 @@
+package com.zhuravishkin.zhuravishkin.stepik.multithreading;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@ToString
+public class T3Point {
+    int x;
+    int y;
+
+    public static void main(String[] args) throws InterruptedException {
+        T3Point p = new T3Point(0, 0);
+        Thread t1 = new Thread(() -> {
+            p.x = 100;
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            p.y = 100;
+        });
+        Thread t2 = new Thread(() -> System.out.println(p));
+        t1.start();
+        t2.start();
+    }
+}
